c34cc0cceb0d6f87fedbed5a110332a202314cd2
Qendrim <qendrim@dua.com>
2021-03-25 22:09:39 +0100

    configured services

---
+++ api/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ubtproduct.Data;
+using UbtProduct.Data;
+
+namespace UbtProduct
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UbtProductContext>(opt => opt.UseMySQL
+            (Configuration.GetConnectionString("UbtProductConnection")));
+            
+            services.AddControllers();
+            services.AddScoped<IUbtProductRepo,SqlUbtProductRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+             app.UseDeveloperExceptionPage();
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
