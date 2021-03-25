cba2de1c75cd7fc25f798ee4c2c46913cb5d8530
Qendrim <qendrim@dua.com>
2021-03-25 22:08:45 +0100

    added innitial  migration for the db

---
+++ api/Migrations/20210321230836_InitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace UbtProduct.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UbtProductsCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UbtProductsCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UbtProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Active = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UbtProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UbtProducts_UbtProductsCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "UbtProductsCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UbtProducts_CategoryId",
+                table: "UbtProducts",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UbtProducts");
+
+            migrationBuilder.DropTable(
+                name: "UbtProductsCategories");
+        }
+    }
+}
